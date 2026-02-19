16b76f28e2592c690b4e06ca629a660ab36e6c27
urballerr <arturbakasov1@gnmail.com>
2026-02-19 19:43:16 +0300

    Добавил интенфейс и реализацию без реализации

---
+++ ReminderApp.Core/Repositories/Implementations/Repository.cs
@@ -0,0 +1,33 @@
+﻿using ReminderApp.Core.Entities.Models;
+using ReminderApp.Core.Repositories.Interfaces;
+
+namespace ReminderApp.Core.Repositories.Implementations
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+        public Task<T> CreateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
